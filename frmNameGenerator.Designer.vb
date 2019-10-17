6a270ce0cc17246bebc9e0423bccd0fa311f1965
marzxxx90 <junmar_honor@ymail.com>
2019-10-17 16:35:49 +0800

    Update

---
+++ frmNameGenerator.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmNameGenerator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmNameGenerator))
+        Me.lvGenerate = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lvGenerate
+        '
+        Me.lvGenerate.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lvGenerate.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.lvGenerate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lvGenerate.FullRowSelect = True
+        Me.lvGenerate.GridLines = True
+        Me.lvGenerate.Location = New System.Drawing.Point(12, 12)
+        Me.lvGenerate.Name = "lvGenerate"
+        Me.lvGenerate.Size = New System.Drawing.Size(581, 488)
+        Me.lvGenerate.TabIndex = 1
+        Me.lvGenerate.UseCompatibleStateImageBehavior = False
+        Me.lvGenerate.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Number"
+        Me.ColumnHeader1.Width = 73
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Name"
+        Me.ColumnHeader2.Width = 514
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnGenerate.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGenerate.Location = New System.Drawing.Point(599, 12)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(144, 488)
+        Me.btnGenerate.TabIndex = 2
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'frmNameGenerator
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(749, 512)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.lvGenerate)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "frmNameGenerator"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Name Picker"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lvGenerate As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents btnGenerate As System.Windows.Forms.Button
+
+End Class
---
+++ frmNameGenerator.vb
@@ -0,0 +1,44 @@
+﻿Public Class frmNameGenerator
+    Private CollctNamePath As String = Application.StartupPath & "\NameList.txt"
+    Private objHash As Hashtable
+    Private NameCollection() As String
+    Private RandomCollection As New List(Of String)
+
+    Private Sub btnGenerate_Click(sender As System.Object, e As System.EventArgs) Handles btnGenerate.Click
+        Dim rand As New Random()
+        Dim RandomValue As Integer
+
+        lvGenerate.Items.Clear()
+
+        For i As Integer = 0 To 9
+DoRandomValue:
+            RandomValue = rand.Next(0, NameCollection.Length - 1)
+            'While CheckIfExistNumber(RandomValue)
+            '    RandomValue = rand.Next(0, NameCollection.Length - 1)
+            'End While
+
+            RandomValue += 1
+            If CheckIfExistNumber(RandomValue) = True Then GoTo DoRandomValue
+
+            RandomCollection.Add(RandomValue)
+            Dim lv As ListViewItem = lvGenerate.Items.Add(RandomValue)
+            lv.SubItems.Add(NameCollection(RandomValue))
+            Application.DoEvents()
+        Next
+    End Sub
+
+    Private Sub frmNameGenerator_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.Text = Me.Text & " | " & Me.GetType.Assembly.GetName.Version.ToString
+        If System.IO.File.Exists(CollctNamePath) = True Then
+            NameCollection = IO.File.ReadAllLines(CollctNamePath)
+        Else
+            MsgBox("Could find Name List", MsgBoxStyle.Critical, "Error")
+        End If
+    End Sub
+
+    Private Function CheckIfExistNumber(ByVal intNumber As Integer)
+        If RandomCollection.Contains(intNumber) Then Return True
+
+        Return False
+    End Function
+End Class
